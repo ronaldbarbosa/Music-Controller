b522d8e5881b2f117bf3eb290120fdccfc82c750
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-02-22 21:00:05 -0300

    Django & React Tutorial #2 - Django REST Framework

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 
 # Create your views here.
 
-
-def main(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer 
